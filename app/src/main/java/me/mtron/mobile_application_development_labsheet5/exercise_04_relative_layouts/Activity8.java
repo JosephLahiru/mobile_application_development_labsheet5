5c481af5ac06608d1681995b0cd5ffbfc1ccb38e
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-21 15:33:54 +0530

    Started exercise_04_relative_layouts

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet5/exercise_04_relative_layouts/Activity8.java
@@ -0,0 +1,16 @@
+package me.mtron.mobile_application_development_labsheet5.exercise_04_relative_layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.mtron.mobile_application_development_labsheet5.R;
+
+public class Activity8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_8);
+    }
+}
