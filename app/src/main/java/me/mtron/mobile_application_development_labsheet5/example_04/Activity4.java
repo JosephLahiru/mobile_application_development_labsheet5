e8e47457fda56d9c9f08cf1a6165f680e1e61a03
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-21 14:37:51 +0530

    Finished example_04

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet5/example_04/Activity4.java
@@ -0,0 +1,16 @@
+package me.mtron.mobile_application_development_labsheet5.example_04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.mtron.mobile_application_development_labsheet5.R;
+
+public class Activity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_4);
+    }
+}
