d1f2f7fe248f441e2b460e8804e3aa3d683baf7b
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-21 15:03:04 +0530

    Started exercise_02_linear_layouts

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet5/exercise_02_linear_layouts/Activity6.java
@@ -0,0 +1,16 @@
+package me.mtron.mobile_application_development_labsheet5.exercise_02_linear_layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.mtron.mobile_application_development_labsheet5.R;
+
+public class Activity6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_6);
+    }
+}
