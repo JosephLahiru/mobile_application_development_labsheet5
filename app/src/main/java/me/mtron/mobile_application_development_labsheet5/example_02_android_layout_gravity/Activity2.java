dc78f607e1f060f7f0104bbbefd14fc29608bb83
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-21 13:48:14 +0530

    Started package example_02_android_layout_gravity

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet5/example_02_android_layout_gravity/Activity2.java
@@ -0,0 +1,16 @@
+package me.mtron.mobile_application_development_labsheet5.example_02_android_layout_gravity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.mtron.mobile_application_development_labsheet5.R;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
