7f6994c0931d7fb26cc0ed60ca22dbf28a6fb78b
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-21 13:56:14 +0530

    Started example_03_android_orientation

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet5/example_03_android_orientation/Activity3.java
@@ -0,0 +1,16 @@
+package me.mtron.mobile_application_development_labsheet5.example_03_android_orientation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.mtron.mobile_application_development_labsheet5.R;
+
+public class Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+    }
+}
