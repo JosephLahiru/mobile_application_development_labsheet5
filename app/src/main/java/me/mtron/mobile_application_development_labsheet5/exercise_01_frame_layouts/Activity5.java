2adc20664aebf63ed412552990fc255580de67f4
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-21 14:39:43 +0530

    Created exercise_01_frame_layouts

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet5/exercise_01_frame_layouts/Activity5.java
@@ -0,0 +1,16 @@
+package me.mtron.mobile_application_development_labsheet5.exercise_01_frame_layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.mtron.mobile_application_development_labsheet5.R;
+
+public class Activity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_5);
+    }
+}
